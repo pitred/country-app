9f326e0fd588ca72957fe030a88642e7abd71e10
Piotr Nowinski <pitred007git@gmail.com>
2019-08-09 00:09:22 +0200

    Add 28.4

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
+import routes from './routes';
+
+// zadanie 28.3
+//import DevTools from './DevTools';
+//import { getCountries } from './actions/actions-countries';
 
 render(
    <Provider store={store}>
-      <div>
-         <h1>Inicjalizacja projektu</h1>
-         <DevTools />
-      </div>
+      <Router history={hashHistory} routes={routes} />
    </Provider>,
    document.getElementById('root')
 );
-store.dispatch(getCountries());
+
+// zadanie 28.3
+//store.dispatch(getCountries());
---
+++ src/presentional/contact.component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Contact = () => <h1>Street: Route 66</h1>;
+
+export default Contact;
